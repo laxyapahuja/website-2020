8abee3117e7c06b7ba058bf72f237833566d2a6c
Anshul Saha <anshulsaha15@gmail.com>
2021-03-08 00:59:10 +0530

    responsiveness

--- src/js/index.js
+++ src/js/index.js
@@ -13,21 +13,21 @@ codeProjectsCardsInnerHTML = ''
 openSourceCardsInnerHTML = ''
 
 function inflator(project, boolean) {
-    string = `<div style='width: 400px; background-color: white; height: 450px; border-radius: 20px; position: relative; box-shadow: 0px 9px 104px -69px rgb(0 0 0 / 50%);'>
-                    <img src="${project.thumbnail}" width='400px' style='border-radius: 20px 20px 0px 0px;'>
-                    <div style="padding-left: 15px; padding-right:15px;">
-                    <h1 class="inter" style="margin-top: 10px; font-size: 25px;">${project.title} (${project.year})</h1>
-                    <p class="helvetica" style="margin-top: 10px; font-size: 20px;">${project.description}</p>
-                    <div id="links" style="position: absolute; display: flex; flex-direction: row; bottom: 0; margin-bottom: 25px; ">`.toString()
+    string = `<div class="inflator-card">
+                    <img src="${project.thumbnail}" class="inflator-image">
+                    <div class="inflator-div">
+                    <h1 class="inter inflator-h1">${project.title} (${project.year})</h1>
+                    <p class="helvetica inflator-p">${project.description}</p>
+                    <div id="links" class="inflator-link">`.toString()
     if (project.project) {
         string = string + `<a href="${project.project}" target="_blank">
-        <p class="inter" style="font-size: 25px; margin-right: 10px;">Project</p>
+        <p class="inter inflator-boolean" style="margin-right: 10px;">Project</p>
     </a>`.toString()
     }
 
     if (project.media) {
         string = string + `<a href="${project.media}" target="_blank">
-        <p class="inter" style="font-size: 25px;">${boolean}</p>
+        <p class="inter inflator-boolean">${boolean}</p>
     </a>`.toString()
     }
     string = string + `</div></div></div>`.toString()
